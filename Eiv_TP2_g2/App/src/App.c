382e7c0fb791c9fb413b10f4a45702106bef4047
Nicolas Miguel Maximiliano Reyes <maximiliano.reyes17@gmail.com>
2021-06-29 00:21:28 -0300

    comentarios en antirrebote

--- Eiv_TP2_g2/App/src/App.c
+++ Eiv_TP2_g2/App/src/App.c
@@ -50,11 +50,11 @@ void setup(void) {
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-	if(GPIO_Pin==pulsador_Pin && !banderaPulsador){
-		offsetPalabra++;
-		banderaPulsador=true;
-		contador=50;
-		if(offsetPalabra>7){
+	if(GPIO_Pin==pulsador_Pin && !banderaPulsador){//verifico si estoy presionandoPINy elestadodela bandera
+		offsetPalabra++;					//incremento palabra
+		banderaPulsador=true;				//cambio valor bandera
+		contador=1;							//inicio contador
+		if(offsetPalabra>7){				//verificacion de desborde
 			offsetPalabra=0;
 		}
 	}
